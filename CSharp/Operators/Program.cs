8f605877cefaff0254f9fbf45827d3e3746c0938
kmw7691 <jun381414@gmail.com>
2022-12-16 21:45:07 +0900

    Array, LoopStatement
    Array
    
    LoopStatement
    while
    for

---
+++ CSharp/Operators/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 14;
+            int b = 5;
+            int c = 0;
+
+            // 산술 연산
+            // 더하기, 빼기, 나누기, 곱하기, 나머지
+            //==================================================================
+
+            // 더하기
+            c = a + b;
+            Console.WriteLine(c);
+
+            // 빼기
+            c = a - b;
+            Console.WriteLine(c);
+
+            // 나누기 - 정수연산시 나머지 버림
+            c = a / b;
+            Console.WriteLine(c);
+
+            // 곱하기
+            c = a * b;
+            Console.WriteLine(c);
+
+            // 나머지 - 실수 연산시 정수연산과 동일함
+            c = a % b;
+            Console.WriteLine(c);
+
+            // 증감 연산
+            // 증가, 감소 연산자
+            //==================================================================
+
+            // 증가
+            ++c; // 전위연산 // c = c + 1;
+            c++; // 후위연산
+
+            c = 0;
+            Console.WriteLine(++c); // result : 1
+            Console.WriteLine(c++); // result : 0
+
+            // 감소
+            --c; // c = c - 1;
+            c--;
+
+            // 관계 연산
+            // 같음, 다름, 크기 비교 연산
+            //==================================================================
+
+            // 같음
+            Console.WriteLine(a == b);
+
+            // 다름
+            Console.WriteLine(a != b);
+
+            // 크다
+            Console.WriteLine(a > b);
+
+            // 작다
+            Console.WriteLine(a < b);
+
+            // 크거나 같다
+            Console.WriteLine(a >= b);
+
+            // 작거나 같다
+            Console.WriteLine(a <= b);
+
+            // 대입 연산
+            // (더해서, 빼서, 나누어서, 곱해서, 나머지구해서) 대입
+            //==================================================================
+
+            c = 20;
+            Console.WriteLine($"C value = {c}");
+            c += b;
+            Console.WriteLine(c);
+            c -= b;
+            Console.WriteLine(c);
+            c /= b;
+            Console.WriteLine(c);
+            c *= b;
+            Console.WriteLine(c);
+            c %= b;
+            Console.WriteLine(c);
+
+            // 논리 연산
+            // 논리형의 피연산자들을 대상으로 연산수행
+            // or, and, xor, not
+            //==================================================================
+
+            bool A = true;
+            bool B = false;
+
+            // or
+            // A 와 B 둘중 하나라도 true 이면 true 반환, 나머지경우 false 반환
+            Console.WriteLine(A | B);
+
+            // and
+            // A 와 B 둘 다 true 이면 true 반환 ,나머지경우 false 반환
+            Console.WriteLine(A & B);
+
+            // xor
+            // A 와 B 둘중 하나만 true 일때 true 반환, 나머지경우 false 반환
+            Console.WriteLine(A ^ B);
+
+            // not
+            // 피연산자 값을 반전
+            Console.WriteLine(!A);
+        }
+    }
+}
---
+++ git/FirstProgram/LoopStatements/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace LoopStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // while 문
+            // 형태
+            //while (반복할 조건)
+            //{
+            // 반복할 내용
+            //}
+            // 조건이 항상 참인 반복문 : 무한루프
+
+            int[] arr = new int[100];
+            int count = 0;
+            while (count < 100)
+            {
+                arr[count] = count++;
+                Console.WriteLine(arr[count - 1]);
+            }
+
+            // do while 문
+            // 한번 일단 실행하고 반복조건 체크
+            // do
+            // {
+            //
+            // } while (반복할 조건);
+
+            do
+            {
+                count++;
+                Console.WriteLine($"[Do_While] : {count}");
+            } while (count < 100);
+
+            // for 문
+            //for (시작 시 한번 수행할 내용; 반복할 조건; 반복할 내용이 한번 끝날 때마다 수행할 내용)
+            //{
+            //
+            //}
+
+
+            for (int i = 0; i < arr.Length; i += 2)
+            {
+                arr[i] = i;
+            }
+
+            string name = "김아무개";
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            int[][] arr_arr_int = new int[3][];
+            arr_arr_int[0] = new int[2];
+            arr_arr_int[1] = new int[3];
+            arr_arr_int[2] = new int[4];
+
+            for (int i = 0; i < arr_arr_int.Length; i++)
+            {
+                for (int j = 0; j < arr_arr_int.Length; j++)
+                {
+                    arr_arr_int[i][j] = 1;
+                }
+            }
+        }
+    }
+}
